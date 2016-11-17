8296525c01c06e95fd708265b9763f55424d4a0e
Jessica Bohn <bohn.16@wright.edu>
2016-11-17 02:25:23 -0600

    Update migration to include url column
    The original migration for the projects table was missing a column
    for the project's url, so after rolling back the database this
    column was added to the original migration file. The url column is
    a string type since each url is less than 255 characters.

--- db/migrate/20161117081422_create_projects.rb
+++ db/migrate/20161117081422_create_projects.rb
@@ -3,6 +3,7 @@ class CreateProjects < ActiveRecord::Migration[5.0]
     create_table :projects do |t|
       t.string :title
       t.text :description
+      t.string :url
 
       t.timestamps(null: false)
     end
