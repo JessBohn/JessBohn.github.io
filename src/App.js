57598ffe4658d45a25ca858ff66e48ae5fccce5c
Jessica Bohn <bohn.16@wright.edu>
2018-01-18 05:57:46 -0600

    Insert new font and animation to page header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 //import logo from './logo.svg';
 import './App.css';
 import hello from './images/hello.png';
@@ -9,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={hello} className="App-logo" alt="logo" />
-          <h1 className="App-title">Jessica Bohn</h1>
+          <h1 className="App-title"><Typist avgTypingDelay={125}>Jessica Bohn</Typist></h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
