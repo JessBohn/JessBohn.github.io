49fc6595a64d04ac1db549cdccc13fceda336b22
Jessica Bohn <bohn.16@wright.edu>
2018-01-18 02:18:58 -0600

    Change header and title to my own name (Jessica Bohn)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import hello from './images/hello.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={hello} className="App-logo" alt="logo" />
+          <h1 className="App-title">Jessica Bohn</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
