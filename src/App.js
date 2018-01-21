1b445c385d54c17b661b23e8df77d6f685dd502a
Jessica Bohn <bohn.16@wright.edu>
2018-01-21 05:24:10 -0600

    Add second header for my job title(s)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ class App extends Component {
         <header className="App-header">
           <img src={hello} className="App-logo" alt="logo" />
           <h1 className="App-title"><Typist avgTypingDelay={125}>Jessica Bohn</Typist></h1>
+          <h2 className="Job-title"><Typist startDelay={1500} cursor={{
+              show: true,
+              hideWhenDone:true,
+              hideWhenDoneDelay: 500
+            }}>Web & Mobile Application Developer | Mechanical Engineer</Typist></h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
